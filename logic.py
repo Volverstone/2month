70de332174af62c1201a0bd885407be6ab84f00f
Volverstone <tilek37@mail.ru>
2024-07-16 12:57:04 +0600

    added files

---
+++ logic.py
@@ -0,0 +1,26 @@
+from random import choice
+from decouple import config
+
+
+
+start_capital = int(config("START_CAPITAL"))
+random_num = int(choice(config("DIAPASONE")))
+
+
+def game(bet,number):
+
+    capital = start_capital
+    print(f"макс баланс {start_capital}")
+    random_num = int(choice(config("DIAPASONE")))
+
+    capital -= bet
+    print(f"текущий капитал c учетом ставки {capital}")
+
+    if number == random_num:
+        bet_win = bet * 2
+        print(f"you win {bet_win}")
+        capital+=bet_win
+    elif number != random_num:
+        bet = 0
+        print("you lose")
+
